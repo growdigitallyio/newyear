64cf5b99aac5ee87fb3de0b62bad3a2300f39f71
saranaga <sarangasama@gmail.com>
2026-04-13 10:03:40 +0530

    add music player component with greeting overlay and background audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import MusicPlayer from './components/MusicPlayer'
 
 const App = () => {
   return (
     <>
+      <MusicPlayer />
       <Routes>
         <Route path='/' element={<HomePage />} />
       </Routes>
